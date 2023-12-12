15591e81e2744574d78df53566e53b309429c96f
AbhishiktSinha <133574794+AbhishiktSinha@users.noreply.github.com>
2023-12-12 16:43:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const container = document.querySelector(".container");
+
+for(let i = 0; i < 32; i++) {
+	for(let j = 0; j < 25; j++) {
+		const square = document.createElement("div");
+		square.classList.add("square");
+
+		let colornum = Math.floor(Math.random()*5) + 1;
+		square.classList.add(`color-${colornum}`);
+
+		container.append(square);
+	}
+}
